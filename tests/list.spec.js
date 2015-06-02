9012c27bf91b0e949082b78f0c7eae057c7ffcac
Oksana Perun <Oksana_Perun@epam.com>
2015-06-02 19:55:22 +0300

    Tests refactoring

--- tests/list.spec.js
+++ tests/list.spec.js
@@ -1,152 +1,152 @@
 var assert = require('chai').assert;
 
-var addPostButton = element(by.id('addPostBtn'));
+describe('Blog', function () {
+    beforeEach(function () {
+        browser.get('http://' + browser.params.server + ':' + browser.params.port);
+    });
 
-function openConfirmationDeletingDialog (postPosition) {
-  var deletePostButton = element.all(by.css('[title="To delete post"]')).get(postPosition);
+    it('should have posts by default', function () {
+        element.all(by.repeater('post in posts')).then(function (posts) {
+            assert.equal(posts.length, 2, 'Check count of posts by default');
+        });
+    });
 
-  deletePostButton.click();
-
-  var modalWindow = element(by.css('.modal-dialog'));
-
-  modalWindow.isDisplayed();
-};
-
-describe('Blog', function() {
-	beforeEach(function () {
-		browser.get('http://' + browser.params.server + ':' + browser.params.port);
-	});
-
-  	it('should have posts by default', function() {
-  		element.all(by.repeater('post in posts')).then(function (posts) {
-			  assert.equal(posts.length, 2, 'Check count of posts by default');
-  		});
-  	});
-
-  	it('first post in the list should have correct title', function() {
-  		element.all(by.css('.blog-post-title')).get(0).getText(function (title) {
-  			assert.equal(title, 'This is a title 2', 'Check title of first post in the list');
-  		});		
-  	});
+    it('first post in the list should have correct title', function () {
+        element.all(by.css('.blog-post-title')).get(0).getText(function (title) {
+            assert.equal(title, 'This is a title 2', 'Check title of first post in the list');
+        });
+    });
 
     it('post should be added', function () {
-      var countOfPostsBeforeAdding = 0;
+        var countOfPostsBeforeAdding = 0;
 
-      element.all(by.repeater('post in posts')).then(function (posts) {
-        countOfPostsBeforeAdding = posts.length;
-      });
+        element.all(by.repeater('post in posts')).then(function (posts) {
+            countOfPostsBeforeAdding = posts.length;
+        });
 
-      addPostButton.click();
+        openFormToAddPost();
 
-      var postForm = element(by.css('.form-horizontal'));
+        var title = element(by.model('post.title')),
+            text = element(by.model('post.text')),
+            addButton = element(by.buttonText('Add'));
 
-      postForm.isDisplayed();
+        title.clear().sendKeys('This is a title of new post');
+        text.clear().sendKeys('This is a text of my new post');
+        addButton.click();
 
-      var title = element(by.model('post.title')),
-          text = element(by.model('post.text')),
-          addButton = element(by.buttonText('Add'));
-
-      title.clear().sendKeys('This is a title of new post');
-      text.clear().sendKeys('This is a text of my new post');
-      addButton.click();
-
-      element.all(by.repeater('post in posts')).then(function (posts) {
-        assert.equal(posts.length, countOfPostsBeforeAdding + 1, 'Check count of posts after adding new one');
-      });
+        element.all(by.repeater('post in posts')).then(function (posts) {
+            assert.equal(posts.length, countOfPostsBeforeAdding + 1, 'Check count of posts after adding new one');
+        });
     });
 
-    it('post should not be added without title', function() {
-      addPostButton.click();
+    it('post should not be added without title', function () {
+        openFormToAddPost();
 
-      var postForm = element(by.css('.form-horizontal'));
+        var text = element(by.model('post.text')),
+            addButton = element(by.buttonText('Add'));
 
-      postForm.isDisplayed();
+        text.clear().sendKeys('This is test text');
+        addButton.click();
 
-      var text = element(by.model('post.text')),
-          addButton = element(by.buttonText('Add'));
+        var errorMessage = element(by.css('.required-message'));
 
-      text.clear().sendKeys('This is test text');
-      addButton.click();
-
-      var errorMessage = element(by.css('.required-message'));
-
-      assert.ok(errorMessage.isDisplayed(), 'Check error message is displayed');
-      assert.ok(postForm.isDisplayed(), 'Check post form is not hidden');
+        assert.ok(errorMessage.isDisplayed(), 'Check error message is displayed');
+        assert.ok(text.isDisplayed(), 'Check post form is not hidden');
     });
 
-    it('post details should be pre-populated when updating', function() {
-      var editFirstPostButton = element.all(by.css('[title="To edit post"]')).get(0);
+    it('post details should be pre-populated when updating', function () {
+        var editFirstPostButton = element.all(by.css('[title="To edit post"]')).get(0);
 
-      editFirstPostButton.click();
+        editFirstPostButton.click();
 
-      var postForm = element(by.css('.form-horizontal'));
+        var postForm = element(by.css('.form-horizontal'));
 
-      postForm.isDisplayed();
+        postForm.isDisplayed();
 
-      element(by.model('post.title')).getAttribute('value').then(function (title) {
-        assert.equal(title, 'This is a title 2', 'Check post title is pre-populated when updating')
-      });
+        element(by.model('post.title')).getAttribute('value').then(function (title) {
+            assert.equal(title, 'This is a title 2', 'Check post title is pre-populated when updating')
+        });
 
-      element(by.model('post.text')).getAttribute('value').then(function (text) {
-        assert.equal(text, 'This is my second blog post', 'Check post text is pre-populated')
-      });
+        element(by.model('post.text')).getAttribute('value').then(function (text) {
+            assert.equal(text, 'This is my second blog post', 'Check post text is pre-populated')
+        });
     });
 
-    it('post should be updated', function() {
-      var editSecondPostButton = element.all(by.css('[title="To edit post"]')).get(1);
+    it('post should be updated', function () {
+        var editSecondPostButton = element.all(by.css('[title="To edit post"]')).get(1);
 
-      editSecondPostButton.click();
+        editSecondPostButton.click();
 
-      var postForm = element(by.css('.form-horizontal'));
+        var postForm = element(by.css('.form-horizontal'));
 
-      postForm.isDisplayed();
+        postForm.isDisplayed();
 
-      var title = element(by.model('post.title')),
-          text = element(by.model('post.text')),
-          updateButton = element(by.buttonText('Update'));
+        var title = element(by.model('post.title')),
+            text = element(by.model('post.text')),
+            updateButton = element(by.buttonText('Update'));
 
-      title.clear().sendKeys('This is a title updated');
-      text.clear().sendKeys('This is my first blog post updated');
-      updateButton.click();
+        title.clear().sendKeys('This is a title updated');
+        text.clear().sendKeys('This is my first blog post updated');
+        updateButton.click();
 
-      element.all(by.css('.blog-post-title')).get(1).getText(function (title) {
-        assert.equal(title, 'This is a title updated', 'Check title of the post after updating');
-      });
+        element.all(by.css('.blog-post-title')).get(1).getText(function (title) {
+            assert.equal(title, 'This is a title updated', 'Check title of the post after updating');
+        });
     });
 
-    it('post should not be deleted', function() {
-      var countOfPosts = 0;
+    it('post should not be deleted', function () {
+        var countOfPosts = 0;
 
-      element.all(by.repeater('post in posts')).then(function (posts) {
-        countOfPosts = posts.length;
-      });
+        element.all(by.repeater('post in posts')).then(function (posts) {
+            countOfPosts = posts.length;
+        });
 
-      openConfirmationDeletingDialog(1);
+        openConfirmationDeletingDialog(1);
 
-      var rejectDeletingButton = element(by.buttonText('No'));
+        var rejectDeletingButton = element(by.buttonText('No'));
 
-      rejectDeletingButton.click();
+        rejectDeletingButton.click();
 
-      element.all(by.repeater('post in posts')).then(function (posts) {
-        assert.equal(posts.length, countOfPosts, 'Check count of posts after rejecting of deleting');
-      });   
+        element.all(by.repeater('post in posts')).then(function (posts) {
+            assert.equal(posts.length, countOfPosts, 'Check count of posts after rejecting of deleting');
+        });
     });
 
-    it('post should be deleted', function() {
-      var countOfPostsBeforeDeleting = 0;
+    it('post should be deleted', function () {
+        var countOfPostsBeforeDeleting = 0;
 
-      element.all(by.repeater('post in posts')).then(function (posts) {
-        countOfPostsBeforeDeleting = posts.length;
-      });  
+        element.all(by.repeater('post in posts')).then(function (posts) {
+            countOfPostsBeforeDeleting = posts.length;
+        });
 
-      openConfirmationDeletingDialog(1);
+        openConfirmationDeletingDialog(1);
 
         var confirmDeletingButton = element(by.buttonText('Yes'));
 
         confirmDeletingButton.click();
 
         element.all(by.repeater('post in posts')).then(function (posts) {
-          assert.equal(posts.length, countOfPostsBeforeDeleting - 1, 'Check count of posts after confirmation of deleting');
+            assert.equal(posts.length, countOfPostsBeforeDeleting - 1, 'Check count of posts after confirmation of deleting');
         });
     });
 });
+
+function openFormToAddPost() {
+    var addPostButton = element(by.id('addPostBtn'));
+
+    addPostButton.click();
+
+    var postForm = element(by.css('.form-horizontal'));
+
+    postForm.isDisplayed();
+};
+
+function openConfirmationDeletingDialog(postPosition) {
+    var deletePostButton = element.all(by.css('[title="To delete post"]')).get(postPosition);
+
+    deletePostButton.click();
+
+    var modalWindow = element(by.css('.modal-dialog'));
+
+    modalWindow.isDisplayed();
+};
--- tests/post.spec.js
+++ tests/post.spec.js
@@ -1,147 +1,151 @@
 var assert = require('chai').assert;
 
-var addCommentButton = element(by.buttonText('Add comment')),
-    firstPostTitle = element.all(by.css('.blog-post-title')).get(0);
+describe('Blog post page', function () {
 
-describe('Blog post page', function() {
-
-	before(function () {
-		browser.get('http://' + browser.params.server + ':' + browser.params.port);
-	});
+    before(function () {
+        browser.get('http://' + browser.params.server + ':' + browser.params.port);
+    });
 
     it('should be opened', function () {
-      firstPostTitle.click();
+        var firstPostTitle = element.all(by.css('.blog-post-title')).get(0);
 
-      element.all(by.xpath('//h2[contains(text(), "Comments")]')).then(function (header) {
-        assert.ok((header.length > 0), 'Comments header should be displayed on post page');
-      });
+        firstPostTitle.click();
 
-      describe('Blog post', function () {
-        beforeEach(function () {
-          browser.get('http://' + browser.params.server + ':' + browser.params.port);
-          firstPostTitle.click();
+        element.all(by.xpath('//h2[contains(text(), "Comments")]')).then(function (header) {
+            assert.ok((header.length > 0), 'Comments header should be displayed on post page');
         });
 
-        it('should have details', function() {
-          element(by.css('.post-title')).getText().then(function (title) {
-            assert.equal(title, 'This is a title 2', 'Check title of the post');
-          });
+        describe('Blog post', function () {
+            beforeEach(function () {
+                browser.get('http://' + browser.params.server + ':' + browser.params.port);
 
-          element(by.css('.post-text')).getText().then(function (text) {
-            assert.equal(text, 'This is my second blog post', 'Check text of the post');
-          });
+                var firstPostTitle = element.all(by.css('.blog-post-title')).get(0);
+
+                firstPostTitle.click();
+            });
+
+            it('should have details', function () {
+                element(by.css('.post-title')).getText().then(function (title) {
+                    assert.equal(title, 'This is a title 2', 'Check title of the post');
+                });
+
+                element(by.css('.post-text')).getText().then(function (text) {
+                    assert.equal(text, 'This is my second blog post', 'Check text of the post');
+                });
+            });
+
+            it('should have a comment by default', function () {
+                element.all(by.repeater('comment in comments')).then(function (comments) {
+                    assert.equal(comments.length, 1, 'Check count of comments by default');
+                });
+            });
+
+            it('comment should be added', function () {
+                var countOfCommentsBeforeAdding = 0;
+
+                element.all(by.repeater('comment in comments')).then(function (comments) {
+                    countOfCommentsBeforeAdding = comments.length;
+                });
+
+                openFormToAddComment();
+
+                var summary = element(by.model('comment.summary')),
+                    text = element(by.model('comment.text')),
+                    postButton = element(by.buttonText('Post'));
+
+                summary.clear().sendKeys('This is a summary of my new comment');
+                text.clear().sendKeys('This is a text of my new comment');
+                postButton.click();
+
+                element.all(by.repeater('comment in comments')).then(function (comments) {
+                    assert.equal(comments.length, countOfCommentsBeforeAdding + 1, 'Check count of comments after adding new one');
+                });
+
+                element(by.css('.comment-badge')).getText().then(function (commentsCount) {
+                    assert.equal(commentsCount, countOfCommentsBeforeAdding + 1, 'Check comments count badge is increased');
+                });
+            });
+
+            it('comment should not be added without summary', function () {
+                openFormToAddComment();
+
+                var text = element(by.model('comment.text')),
+                    postButton = element(by.buttonText('Post'));
+
+                text.clear().sendKeys('This is test text');
+                postButton.click();
+
+                var errorMessage = element(by.css('.required-message'));
+
+                assert.ok(errorMessage.isDisplayed(), 'Check error message is displayed');
+                assert.ok(text.isDisplayed(), 'Check comment form is not hidden');
+            });
+
+            it('comment details should be pre-populated when updating', function () {
+                var editFirstCommentButton = element.all(by.css('[title="To edit comment"]')).get(0);
+
+                editFirstCommentButton.click();
+
+                var commentForm = element(by.css('.form-horizontal'));
+
+                commentForm.isDisplayed();
+
+                element(by.model('comment.summary')).getAttribute('value').then(function (summary) {
+                    assert.equal(summary, 'Looks good', 'Check comment summary is pre-populated when updating')
+                });
+
+                element(by.model('comment.text')).getAttribute('value').then(function (text) {
+                    assert.equal(text, 'This is a comment', 'Check comment text is pre-populated')
+                });
+            });
+
+            it('comment should be updated', function () {
+                var editFirstCommentButton = element.all(by.css('[title="To edit comment"]')).get(0);
+
+                editFirstCommentButton.click();
+
+                var commentForm = element(by.css('.form-horizontal'));
+
+                commentForm.isDisplayed();
+
+                var summary = element(by.model('comment.summary')),
+                    text = element(by.model('comment.text')),
+                    updateButton = element(by.buttonText('Update'));
+
+                summary.clear().sendKeys('This is a summary updated');
+                text.clear().sendKeys('This is my first blog post comment updated');
+                updateButton.click();
+
+                element.all(by.css('.comment-summary')).get(0).getText(function (summary) {
+                    assert.equal(summary, 'This is a summary updated', 'Check summary of the comment after updating');
+                });
+            });
+
+            it('comment should be deleted', function () {
+                var countOfCommentsBeforeDeleting = 0;
+
+                element.all(by.repeater('comment in comments')).then(function (comments) {
+                    countOfCommentsBeforeDeleting = comments.length;
+                });
+
+                var deleteFirstCommentButton = element.all(by.css('[title="To delete comment"]')).get(0);
+
+                deleteFirstCommentButton.click();
+
+                element.all(by.repeater('comment in comments')).then(function (comments) {
+                    assert.equal(comments.length, countOfCommentsBeforeDeleting - 1, 'Check count of comments after deleting');
+                });
+            });
         });
-
-        it('should have a comment by default', function() {
-          element.all(by.repeater('comment in comments')).then(function (comments) {
-            assert.equal(comments.length, 1, 'Check count of comments by default');
-          });   
-        });
-
-        it('comment should be added', function () {
-          var countOfCommentsBeforeAdding = 0;
-
-          element.all(by.repeater('comment in comments')).then(function (comments) {
-            countOfCommentsBeforeAdding = comments.length;
-          });
-
-          addCommentButton.click();
-
-          var commentForm = element(by.css('.form-horizontal'));
-
-          commentForm.isDisplayed();
-
-          var summary = element(by.model('comment.summary')),
-              text = element(by.model('comment.text')),
-              postButton = element(by.buttonText('Post'));
-
-          summary.clear().sendKeys('This is a summary of my new comment');
-          text.clear().sendKeys('This is a text of my new comment');
-          postButton.click();
-
-          element.all(by.repeater('comment in comments')).then(function (comments) {
-            assert.equal(comments.length, countOfCommentsBeforeAdding + 1, 'Check count of comments after adding new one');
-          });
-
-          element(by.css('.comment-badge')).getText().then(function (commentsCount) {
-            assert.equal(commentsCount, countOfCommentsBeforeAdding + 1, 'Check comments count badge is increased');
-          });
-        });
-
-        it('comment should not be added without summary', function() {
-          addCommentButton.click();
-
-          var commentForm = element(by.css('.form-horizontal'));
-
-          commentForm.isDisplayed();
-
-          var text = element(by.model('comment.text')),
-              postButton = element(by.buttonText('Post'));
-
-          text.clear().sendKeys('This is test text');
-          postButton.click();
-
-          var errorMessage = element(by.css('.required-message'));
-
-          assert.ok(errorMessage.isDisplayed(), 'Check error message is displayed');
-          assert.ok(commentForm.isDisplayed(), 'Check comment form is not hidden');
-        });
-
-        it('comment details should be pre-populated when updating', function() {
-          var editFirstCommentButton = element.all(by.css('[title="To edit comment"]')).get(0);
-
-          editFirstCommentButton.click();
-
-          var commentForm = element(by.css('.form-horizontal'));
-
-          commentForm.isDisplayed();
-
-          element(by.model('comment.summary')).getAttribute('value').then(function (summary) {
-            assert.equal(summary, 'Looks good', 'Check comment summary is pre-populated when updating')
-          });
-
-          element(by.model('comment.text')).getAttribute('value').then(function (text) {
-            assert.equal(text, 'This is a comment', 'Check comment text is pre-populated')
-          });
-        });
-
-        it('comment should be updated', function() {
-          var editFirstCommentButton = element.all(by.css('[title="To edit comment"]')).get(0);
-
-          editFirstCommentButton.click();
-
-          var commentForm = element(by.css('.form-horizontal'));
-
-          commentForm.isDisplayed();
-
-          var summary = element(by.model('comment.summary')),
-              text = element(by.model('comment.text')),
-              updateButton = element(by.buttonText('Update'));
-
-          summary.clear().sendKeys('This is a summary updated');
-          text.clear().sendKeys('This is my first blog post comment updated');
-          updateButton.click();
-
-          element.all(by.css('.comment-summary')).get(0).getText(function (summary) {
-            assert.equal(summary, 'This is a summary updated', 'Check summary of the comment after updating');
-          });
-        });
-
-        it('comment should be deleted', function() {
-          var countOfCommentsBeforeDeleting = 0;
-
-          element.all(by.repeater('comment in comments')).then(function (comments) {
-            countOfCommentsBeforeDeleting = comments.length;
-          });  
-
-          var deleteFirstCommentButton = element.all(by.css('[title="To delete comment"]')).get(0);
-
-          deleteFirstCommentButton.click();
-
-          element.all(by.repeater('comment in comments')).then(function (comments) {
-            assert.equal(comments.length, countOfCommentsBeforeDeleting - 1, 'Check count of comments after deleting');
-         });
-      });
-     });
-  });
+    });
 });
+
+function openFormToAddComment() {
+    var addCommentButton = element(by.buttonText('Add comment'));
+
+    addCommentButton.click();
+
+    var commentForm = element(by.css('.form-horizontal'));
+
+    commentForm.isDisplayed();
+};
